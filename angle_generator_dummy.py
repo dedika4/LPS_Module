6dddf44d294672a64238d8d7f9a193712452e872
dedika4 <dewamahardika499@gmail.com>
2021-03-17 20:52:17 +0700

    Modify periodic sending method from sleep() to use a caller
    Caller use time difference in two points to calculate how long
    time has passed from the reference start point, execute function
    after certain difference and update the reference start point
    after execution

--- angle_generator_dummy.py
+++ angle_generator_dummy.py
@@ -9,15 +9,23 @@ line = connection.channel()
 line.exchange_declare(exchange='beam-angle',
                     exchange_type='fanout')
 
+packet_update_period = 0.5 # in seconds
+
 while True:
+    i = 0
     try:
-        for i in range(0,190,10):
-            data = i
-            line.basic_publish(exchange='beam-angle',
-                            routing_key='',
-                            body=str(data))
-            print(" {} Packet sent".format(data))
-            time.sleep(0.5)
+        t = time.time()
+        while i <= 180:
+            t_now = time.time()
+            if (t_now - t)>= packet_update_period:
+                data = i
+                line.basic_publish(exchange='beam-angle',
+                                routing_key='',
+                                body=str(data))
+                print(" {} Packet sent".format(data))
+                #time.sleep(0.5)
+                i += 10
+                t = time.time()
     except KeyboardInterrupt:
         print('Interrupted')
         try:
